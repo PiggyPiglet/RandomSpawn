8c9090ddc2b3e8f9aeb905107062a5bdd5ea023a
PiggyPiglet <noreply@piggypiglet.me>
2019-05-19 20:49:36 +0800

    somehow these didn't get staged

--- src/main/java/me/piggypiglet/randomspawn/file/types/data/Data.java
+++ src/main/java/me/piggypiglet/randomspawn/file/types/data/Data.java
@@ -1,8 +1,45 @@
 package me.piggypiglet.randomspawn.file.types.data;
 
+import com.google.inject.Inject;
+import me.piggypiglet.randomspawn.file.FileManager;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+
 // ------------------------------
 // Copyright (c) PiggyPiglet 2019
 // https://www.piggypiglet.me
 // ------------------------------
 public final class Data {
+    private FileConfiguration data;
+
+    @Inject
+    public Data(FileManager fileManager) {
+        this.data = fileManager.getConfig("data");
+    }
+
+    public Spawn setSpawn(String name, Location location) {
+        if (location.getWorld() != null) {
+            String prefix = "data.locations." + name + ".";
+            Spawn spawn = Spawn.builder()
+                    .name(name)
+                    .world(location.getWorld().getName())
+                    .x(location.getX())
+                    .y(location.getY())
+                    .z(location.getZ())
+                    .yaw(location.getYaw())
+                    .pitch(location.getPitch())
+                    .build();
+
+            data.set(prefix + "world", spawn.getWorld());
+            data.set(prefix + "x", spawn.getX());
+            data.set(prefix + "y", spawn.getY());
+            data.set(prefix + "z", spawn.getZ());
+            data.set(prefix + "yaw", spawn.getYaw());
+            data.set(prefix + "pitch", spawn.getPitch());
+
+            return spawn;
+        }
+
+        return null;
+    }
 }
--- src/main/java/me/piggypiglet/randomspawn/file/types/data/Spawn.java
+++ src/main/java/me/piggypiglet/randomspawn/file/types/data/Spawn.java
@@ -1,8 +1,19 @@
 package me.piggypiglet.randomspawn.file.types.data;
 
+import lombok.Builder;
+import lombok.Data;
+
 // ------------------------------
 // Copyright (c) PiggyPiglet 2019
 // https://www.piggypiglet.me
 // ------------------------------
+@Builder @Data
 public final class Spawn {
+    private String name;
+    private String world;
+    private double x;
+    private double y;
+    private double z;
+    private float yaw;
+    private float pitch;
 }
