cda954d8f724682852a9a77f3bb918954b990bea
PiggyPiglet <noreply@piggypiglet.me>
2019-05-19 19:15:01 +0800

    Got rid of everything

--- src/main/java/me/piggypiglet/randomspawn/enums/Messages.java
+++
@@ -1,9 +0,0 @@
-package me.piggypiglet.randomspawn.enums;
-
-// ------------------------------
-// Copyright (c) PiggyPiglet 2017
-// https://www.piggypiglet.me
-// ------------------------------
-public enum Messages {
-    PREFIX
-}
